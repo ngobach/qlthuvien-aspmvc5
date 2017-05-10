bec80b208b433358052cd15aac6be95acbacc6a5
Ngô Xuân Bách <thanbaiks@gmail.com>
2017-05-10 15:13:00 +0700

    Fix ticket column types

--- WebQLThuVien/Models/Ticket.cs
+++ WebQLThuVien/Models/Ticket.cs
@@ -9,6 +9,11 @@ namespace WebQLThuVien.Models
 {
     public class Ticket
     {
+        public Ticket()
+        {
+            Books = new List<Book>();
+        }
+
         [Display(Name = "Mã phiếu")]
         [DisplayFormat(DataFormatString = "PM{0:0000}")]
         public int Id { get; set; }
@@ -17,9 +22,11 @@ namespace WebQLThuVien.Models
         [ForeignKey("ReaderId")]
         public virtual Reader Reader { get; set; }
         [Display(Name = "Ngày tạo")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime DateCreated { get; set; }
         [Display(Name = "Ngày trả")]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true, NullDisplayText = "Chưa trả")]
         public DateTime? DateReturned { get; set; }
         [Display(Name = "Sách mượn")]
